b61b5c859a457cfdec856070bfc2c248828e4c3a
tatobrito <gnothisway@hotmail.com>
2025-07-26 17:46:26 -0300

    Add employee management actions for peers, subordinates, and second-level subordinates

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,6 +1,6 @@
 class EmployeesController < ApplicationController
   before_action :set_company
-  before_action :set_employee, only: [:destroy, :set_manager]
+  before_action :set_employee, only: [:destroy, :set_manager, :peers, :subordinates, :second_level_subordinates]
 
   def index
     render json: @company.employees
@@ -32,6 +32,28 @@ class EmployeesController < ApplicationController
     end
   end
 
+  def peers
+    if @employee.manager.nil?
+      # Se o funcionário não tem gestor, peers são outros funcionários sem gestor na mesma empresa
+      peers = @company.employees.where(manager_id: nil).where.not(id: @employee.id)
+    else
+      # Peers são funcionários que tem o mesmo gestor
+      peers = @company.employees.where(manager_id: @employee.manager_id).where.not(id: @employee.id)
+    end
+    render json: peers
+  end
+
+  def subordinates
+    subs = @company.employees.where(manager_id: @employee.id)
+    render json: subs
+  end
+
+  def second_level_subordinates
+    first_level_ids = @company.employees.where(manager_id: @employee.id).pluck(:id)
+    second_level_subs = @company.employees.where(manager_id: first_level_ids)
+    render json: second_level_subs
+  end
+
   private
 
   def set_company
@@ -43,6 +65,6 @@ class EmployeesController < ApplicationController
   end
 
   def employee_params
-      params.require(:employee).permit(:name, :email, :picture, :manager_id, :admin, :superadmin)
+    params.require(:employee).permit(:name, :email, :picture, :manager_id, :admin, :superadmin)
   end
 end
